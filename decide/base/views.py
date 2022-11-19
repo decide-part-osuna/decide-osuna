0d0d59f29cfa99084976c9cde959dbb741f4d6ec
juatalfri <juanmanueltalerofrias@gmail.com>
2022-11-19 12:47:54 +0100

    se ha añadido una vista de inicio y se ha modificado el requirements.txt

--- decide/base/views.py
+++ decide/base/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import json
+from django.views.generic import TemplateView
 
-# Create your views here.
+# def inicio(request):
+#     return render(request,'base.html')
+
+class IndexView(TemplateView):
+    
+    def inicio(request):
+        return render(request,'index.html')
