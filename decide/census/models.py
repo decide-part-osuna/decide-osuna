d10de985bcaa15fff8da850a6d2546ea7021451d
pabsanper <pablitosantospsp@gmail.com>
2022-11-22 23:58:40 +0100

    fim

--- decide/census/models.py
+++ decide/census/models.py
@@ -1,9 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from voting.models import Voting
 
 class Census(models.Model):
-    voting_id = models.PositiveIntegerField()
-    voter_id = models.PositiveIntegerField()
+    name = models.CharField(max_length=50, default='')
+    voting_id = models.ForeignKey(Voting,on_delete=models.CASCADE)
+    voter_id= models.ManyToManyField(User)
 
     class Meta:
-        unique_together = (('voting_id', 'voter_id'),)
+        unique_together = (('name'),('voting_id'),)
+        
+    def __unicode__(self):
+        users = []
+        for voter in self.voter_id.all():
+            users.append(voter.username)
+        return '{}:[{},{}]'.format(self.name,self.voting_id.name,users)
+
