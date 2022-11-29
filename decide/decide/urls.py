f9500b0b1041fec3e1f480dca7f5e6007884f4cd
Antonio Roberto Serrano Mena <80272558+antsermen@users.noreply.github.com>
2022-11-29 11:52:40 +0100

    Revert "Adición del formulario de registro de usuario y botón de autenticación con google"

--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
 
+
 schema_view = get_swagger_view(title='Decide API')
 
 urlpatterns = [
