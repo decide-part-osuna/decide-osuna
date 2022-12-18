bd13f96eac511ac48aa18218b6ca8b5c9a9602cb
pabsanper <pablitosantospsp@gmail.com>
2022-12-18 17:38:44 +0100

    fix: soluciona email password

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -210,5 +210,5 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'decidepartosuna@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
